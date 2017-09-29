102f24f12368feff5091a33e92c1c0a0ac52ecfa
BestDingSheng <727040141@qq.com>
2017-09-29 15:07:41 +0800

    test

--- app.js
+++ app.js
@@ -22,8 +22,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
 app.use('/', index);
 app.use('/api', users);
+
+
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
