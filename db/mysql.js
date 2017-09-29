fa27932f5a4d5bd1965ee145a83277067bed0f75
BestDingSheng <727040141@qq.com>
2017-09-29 17:15:44 +0800

    file change

--- db/mysql.js
+++ db/mysql.js
@@ -30,8 +30,23 @@ let query = function( sql, values ) {
 }
 
 
-// 查找用户
+//  注册 查找用户是否存在
 let getuser = function( value ) {
+  let _sql = "SELECT * FROM user_table WHERE (username)= (?)";
+  return query( _sql, value )
+}
+
+// 数据储存用户
+
+let adduser = function( value ) {
+  // let _sql = "SELECT * FROM user_table WHERE (username)= (?)";
+  let _sql = "insert into user_table (username,password) values (?,?)";
+  return query( _sql, value )
+}
+
+
+// 登陆
+let login = function( value ) {
   let _sql = "SELECT * FROM user_table WHERE (username,password)= (?,?)";
   return query( _sql, value )
 }
@@ -46,14 +61,26 @@ let getAlluser = function(){
 
 // 发表文章
 let insertPost = function( value ) {
-  let _sql = "insert into posts(name,title,content,uid,moment) values(?,?,?,?,?);"
+  let _sql = "insert into post (username,content,date) values(?,?,?)";
   return query( _sql, value )
 }
+
+// 查找所有文章
+
+let getAllPost =  function(value){
+  let _sql = "SELECT * FROM post order by uid desc limit 0,10";
+  return query( _sql )
+}
+
 
 
 module.exports={
 	query,
   getuser,
-  getAlluser
+  adduser,
+  login,
+  getAlluser,
+  insertPost,
+  getAllPost
 
 }
